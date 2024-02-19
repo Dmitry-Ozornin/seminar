6c19bef554a0ef8b639a24f921193b5b2353ae0f
Dmitry Ozornin <ozornindimitry@yandex.ru>
2024-02-19 22:47:49 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 < 0)
+{
+    number1 = number1*(-1);
+}
+int index = - number1;
+while (index <= number1)
+{
+    Console.Write($"{index} ");
+    index++;
+}
