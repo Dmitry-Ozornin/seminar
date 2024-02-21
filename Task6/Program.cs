d4c76ec2bcaa810a6e330103c62c1589c9462496
Dmitry Ozornin <ozornindimitry@yandex.ru>
2024-02-21 20:27:28 +0300

    программа удаления второй цифры через рандом

---
+++ Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на
+// вход трёхзначное число и удаляет вторую
+// цифру этого числа. Запишем через рандом числа.
+
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+int s = num/100*10 + num%10;
+Console.WriteLine(s);
+Console.WriteLine(num/100 + "" + num%10);
