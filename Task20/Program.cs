628f8df5ffaffc5d52fdd5c6b5358e92837f1ccd
Dmitry Ozornin <ozornindimitry@yandex.ru>
2024-03-10 19:18:41 +0300

    Задайте массив символов (тип char []). Создайте строку из символов этого массива.

---
+++ Task20/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив символов (тип char []). Создайте строку из
+// символов этого массива.
+
+string CharOfString(char[] arr) //создание строки
+{
+    string st = "";
+    foreach (var item in arr)
+    {
+        st += item;
+    }
+    return st;
+}
+
+char[] chars = new char[] { 'a', 'b', 'c', 'd'}; //начало
+Console.WriteLine(CharOfString(chars)); // вывод на экран
