80bdaa5adc68869c7f12e20670a1dd9cce845a04
Dmitry Ozornin <ozornindimitry@yandex.ru>
2024-03-14 23:50:55 +0300

    Задайте значение N. Напишите программу, которая выведет  все натуральные числа в промежутке от 1 до N.

---
+++ Task24/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значение N. Напишите программу, которая выведет  все натуральные числа в промежутке от 1 до N.
+
+string GetListNum(int num, int index = 1)
+{
+    if ( index == num)
+    {
+        return Convert.ToString(index);
+    }
+    return index + " " + GetListNum(num, index + 1);
+
+}
+
+
+Console.WriteLine("введите число");
+int num =  Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(GetListNum(num));
