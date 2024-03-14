297ac7c52e181954bfa21d95ff47f7b69feab025
Dmitry Ozornin <ozornindimitry@yandex.ru>
2024-03-15 00:31:40 +0300

    // Считать строку с консоли, содержащую латинские буквы. Вывести на экран согласные буквы этой строки.

---
+++ Task25/Program.cs
@@ -0,0 +1,26 @@
+﻿// Считать строку с консоли, содержащую латинские буквы. Вывести на экран согласные буквы этой строки.
+
+using System.Text; //подключение библиотеки( помогло писать на русском)
+Console.InputEncoding = Encoding.Unicode; // для русского языка
+Console.OutputEncoding = Encoding.Unicode; // для русского языка
+
+
+
+Console.WriteLine("введите слово");
+string str = Console.ReadLine()!;
+
+void Buk(string str)
+{
+    if (str == "")
+    {
+        return;
+    }
+    string vowel = "aoueiAOUEI";
+    if (!vowel.Contains(str[0]))
+    {
+    Console.Write(str[0]);
+    }
+    Buk(str[1..]);   //Hello[2..] получиться llo
+}
+
+Buk(str);
